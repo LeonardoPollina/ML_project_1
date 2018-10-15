6ccb8032d2ae002f2b6622f64f17a468aaa5cab2
Nicola Ischia <nicolaischia95@gmail.com>
2018-10-15 16:43:42 +0200

    added standardize

--- HelperFunctions/common_functions.py
+++ HelperFunctions/common_functions.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+    
+
 def split_data(x, y, ratio, seed=1):
     """
     TODO: GENERALIZE WHEN X IS A MATRIX
